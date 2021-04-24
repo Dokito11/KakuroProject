873888f2aa984dde16af99be4a881ffaedf77fde
Alex <michauda16@gmail.com>
2021-04-24 01:36:47 -0400

    Added Game Class and Cleaning Code a bit

---
+++ src/classes/Game.java
@@ -0,0 +1,46 @@
+package classes;
+
+import java.awt.Font;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class Game {
+
+	public static void input(KeyEvent e, JTextField txtField)
+	{
+		
+		char c = e.getKeyChar();
+		
+		if(c==KeyEvent.VK_BACK_SPACE||c==KeyEvent.VK_DELETE)
+		{
+			return;
+		}
+		
+		if(!Character.isDigit(c)) {
+			e.consume();
+		}
+		else
+		{
+			txtField.setText("");
+			return;
+		}
+		
+	}
+	
+	public static void InitTextfield(JFrame frame, JTextField txtField, int x, int y, int width, int height)
+	{
+		txtField.setFont(new Font("Arial Black", Font.BOLD, 14));
+		txtField.setBounds(x,y,width,height);
+		txtField.setColumns(10);
+		frame.getContentPane().add(txtField);
+		txtField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				input(e, txtField);
+			}
+		});
+	}
+}
